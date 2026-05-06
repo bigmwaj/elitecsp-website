69ba7bc31d28db08c5d09a7b90b9830c60326b1f
Alain Mouafo <alainmouafo@gmail.com>
2026-05-06 11:12:31 -0400

    AP

--- ui-app/src/environments/environment.prod.ts
+++ ui-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://ek7n4dkkde.execute-api.ca-central-1.amazonaws.com/prod',
-  apiKey: 'elite-csp-api-key'
+  apiKey: 'k9Q3WwwI828q0lvPMn5koVtIepZtnkR7wC4klD22'
 };
--- ui-app/src/environments/environment.ts
+++ ui-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://wkz8o24uc4.execute-api.ca-central-1.amazonaws.com/prod',
-  apiKey: 'REPLACE_WITH_DEV_API_KEY'
+  apiKey: 'k9Q3WwwI828q0lvPMn5koVtIepZtnkR7wC4klD22'
 };
